2e0f7071b90e10f1f0a893d8f79d230a223242a0
alexfedsv <alfedsv@yandex.ru>
2026-08-25 08:13:28 +0300

    initial commit

---
+++ ExtractSport/ExtractSport/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RootViewController.swift
+//  ExtractSport
+//
+//  Created by  Alexander Fedoseev on 25.08.2026.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
